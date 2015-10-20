0d0cb27c741cbf5c3a01c02945d368a381c4dc7f
Vasin Wongrassamee <vw214@texel17.doc.ic.ac.uk>
2015-10-20 16:30:10 +0100

    power on. tested

--- src/main/java/ic/doc/camera/Camera.java
+++ src/main/java/ic/doc/camera/Camera.java
@@ -1,17 +1,28 @@
 package ic.doc.camera;
 
-public class Camera {
+public class Camera implements WriteListener{
+
+    Sensor sensor;
+
+    public Camera(Sensor sensor){
+        this.sensor = sensor;
+    }
 
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
-        // not implemented
+        sensor.powerUp();
     }
 
     public void powerOff() {
-       // not implemented
+       sensor.powerDown();
+    }
+
+    @Override
+    public void writeComplete() {
+
     }
 }
 
--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -1,5 +1,6 @@
 package ic.doc.camera;
 
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,9 +10,16 @@ public class CameraTest {
     @Rule
     public JUnitRuleMockery context = new JUnitRuleMockery();
 
+    final Sensor sensor = context.mock(Sensor.class);
+    Camera camera = new Camera(sensor);
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
 
+        context.checking(new Expectations() {{
+            exactly(1).of(sensor).powerUp();
+        }} );
+        camera.powerOn();
          // write your test here
     }
 
